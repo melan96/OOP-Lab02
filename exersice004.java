566ef594ed7a187b0d0b054a37ff7eff7c9a84cb
Melan Rashitha Dias <30628058+melan96@users.noreply.github.com>
2018-02-18 13:00:57 +0530

    Add files via upload

---
+++ exersice004.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class getStreamLine{
+
+	public staic void main(String[] args){
+		
+		
+		InputStreamReader objInput=inputStreamReader(System.in);	
+
+		BufferedReader Bufferedobj = new BufferedReader(objInput);
+		
+		//fetch String		
+		String name;
+		name =Bufferedobj.readLine();
+		
+		//fetch Integer
+		int b = Integer.parseInt(r.readLine);
+		
+	}
+}
